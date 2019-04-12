54b3bcfff5cf4a1e8275054685d01445c39b0db4
Alonso Urrea <45604888+AlonShaqed@users.noreply.github.com>
2019-04-12 12:35:20 -0600

    Update decode.py

--- SocketPython/decode.py
+++ SocketPython/decode.py
@@ -1,4 +1,5 @@
 from code import stringChecksum, is_num_type
+from csv import DATA_LOG, TRANS_LOG
 
 def decodeUpdateString(string):
 	if type(string) is type(""):	
@@ -11,15 +12,16 @@ def decodeUpdateString(string):
 				if len(string) == 35:
 					if string[0] == "u":
 						data = []
-						data.append(string[1:9])
+						data.append(string[1:9].strip())
 						data.append(string[9:17])
 						data.append(string[17:32])
 						data.append(string[23:31])
+						data.append(DATA_LOG)
 
 						if "-" in data[1]:
 							data[1] = data[1].replace("-", "", 1)
 							data[1] = "-" + data[1]
-						for split in data[1:]:
+						for split in data[1:-1]:
 							float(split)
 						data[2] = data[2][0:2] + ":" + data[2][2:4] + ":" + data[2][4:6]
 						data[3] = data[3][0:2] + "-" + data[3][2:4] + "-" + data[3][4:]
@@ -47,8 +49,9 @@ def decodeObserverString(string):
 				if len(string) == 21:
 					if string[0] == "r":
 						data = []
-						data.append(string[1:9])
-						data.append(string[9:17])
+						data.append(string[1:9].strip())
+						data.append(string[9:17].strip())
+						data.append(TRANS_LOG)
 
 						return data
 				else:
@@ -60,4 +63,13 @@ def decodeObserverString(string):
 		except ValueError:
 			print("Error en valores numericos")
 	else:
+		return False
+
+def decode_packet(string):
+	if string[0] == "r":
+		return decodeObserverString(string)
+	elif string[0] == "u":
+		return decodeUpdateString(string)
+	else:
+		print("Codificacion desconocida")
 		return False
