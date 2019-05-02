5677bba6a93b41d692fb5680e052e27bf04d2c78
Alonso Urrea <45604888+AlonShaqed@users.noreply.github.com>
2019-05-02 15:33:36 -0600

    Update server.rb

--- SocketRuby/server.rb
+++ SocketRuby/server.rb
@@ -9,33 +9,29 @@ def receive_from_socket
 	return client.gets
 end
 
-def receive_and_send_from_socket(message)
+def send_to_socket(message)
+	client = $server.accept
 	if message.is_a? String
-		client = $server.accept
-		ret = client.gets
 		client.puts message
-
-		return ret
+	else
+		client.puts "ERROR"
 	end
-
-	return "Invalid message"
 end
 
 
 loop do
 	from_client = receive_from_socket.chomp
 	packet = decode_packet(from_client)
-	
 	unless packet == false or packet == nil
 		if packet.length == 3
-			line = findInLine($DATA_LOG, packet[1])
+			line = findInFile($DATA_LOG, packet[1])
 			if line == "Not found"
 				toSend = codeUpdateString("notfound", 0)
 			else
 				toSend = codeFromLog(line)
 			end
-			receive_and_send_from_socket(toSend)
+			send_to_socket(toSend)
 		end
-		writeToFile(packet[-1], list_to_csv(packet[0...-1]))
+		writeToFile(packet[-1] ,list_to_csv(packet[0...-1]))
 	end
 end
